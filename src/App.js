8d1cdc7c8fb348cbe605f75cf603192b27f50b2e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-12 08:53:21 -0500

    Added logos, started updating files to reference

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

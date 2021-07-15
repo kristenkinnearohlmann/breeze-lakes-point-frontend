13d2946fc87fe59a6550a1882325506d812799ea
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-15 18:11:52 -0500

    Cleaned up Splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,14 +3,12 @@ import logo from '../logo.png';
 
 const Main = () => {
     return (
-        <div className="app">
-        <header className="app-header">
+        <div className="app app-header">
           <img src={logo} className="app-logo" alt="logo" />
           <div>
             <span className="title brand-name">Breeze Lakes Point</span><br/>
             <span className="title">serious fun!</span>
           </div>
-        </header>
       </div>
     )
 }

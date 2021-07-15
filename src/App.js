9a297548f3061374296b47fc6f20993056456e79
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-15 18:08:51 -0500

    Moved content out of App to Main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.png';
+// import logo from './logo.png';
+import Main from './components/Main'
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <div>
-          <span className="title brand-name">Breeze Lakes Point</span><br/>
-          <span className="title">serious fun!</span>
-        </div>
-      </header>
-    </div>
+    <Main />
   );
 }
 

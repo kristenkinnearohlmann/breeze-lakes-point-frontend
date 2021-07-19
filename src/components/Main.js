71cb9df4965caceba3a1b4e9e2c3674d7e7da257
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-18 22:21:10 -0500

    Installed React Router and prepped Main component for use

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Header from './Header'
 import MainContent from './MainContent'
+import { BrowserRouter as Router, Route, Swith } from 'react-router-dom'
 
 const Main = () => {
 
     return (
+      <Router>
         <div>
           <Header />
           <MainContent />       
         </div>
+      </Router>
     )
 }
 

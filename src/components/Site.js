e5ca6d9af5cf64a78047f215ca7911589f557f1e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-20 20:54:42 -0500

    Working to resolve existing state transition error

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Redirect } from 'react-router-dom'
 import Splash from './Splash'
 
 const Site = ({ history }) => {
@@ -14,21 +13,14 @@ const Site = ({ history }) => {
     }, [])
 
     const loadSplashOrMain = history => {
-      if (loading === true) {
-        return <Splash />
-      } else {
         history.push("/home")
-      }
     }
 
     return (
-      <Router>
-        <div>
-          {/* {(loading === true && window.location.pathname === '/') ? <Splash /> : <Main />}       */}
-          {/* {loading === true ? <Splash /> : <Redirect to={"/home"} />} */}
-          {loadSplashOrMain(history)}
-        </div>
-      </Router>
+      <div>
+        {/* {(loading === true) ? <Splash /> : loadSplashOrMain(history)} */}
+        {(loading === true) ? <Splash /> : history.push("/home")}
+      </div>
     )
 }
 

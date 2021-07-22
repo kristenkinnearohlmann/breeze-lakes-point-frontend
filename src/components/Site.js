b55639c570411b656bab0ca2043d0d6b8cd01473
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-21 22:08:19 -0500

    Updated Splash to do simple component display

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,32 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Splash from './Splash'
 
-const Site = ({ history }) => {
-
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setLoading(false)
-      }, 10000)
-
-      return () => {
-        clearTimeout(timer)
-      }
-    }, [])
-
-    const displayContent = () => {
-      if (loading === true) {
-        return <Splash />
-      } else {
-        history.push("/home")
-      }
-    }
+const Site = () => {
 
     return (
       <div>
-        {/* {(loading === true) ? <Splash /> : loadSplashOrMain(history)} */}
-        {displayContent(loading)}
+        <Splash />
       </div>
     )
 }

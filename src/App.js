9ba87257ffa7f28c3086f28e0ef8a1a8e310efe7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-21 22:12:36 -0500

    Cleaned up commented code in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from './components/Header'
-import Splash from './components/Splash'
 import Site from './components/Site'
 import Main from './components/Main'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -11,7 +10,6 @@ function App() {
       <div className="app">
         <Header />
         <Switch>
-          {/* <Route exact path="/" component={Splash} /> */}
           <Route exact path="/" component={Site} />
           <Route exact path="/home" component={Main} />
         </Switch>

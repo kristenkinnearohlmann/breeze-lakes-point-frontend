620e8a1010c75594cd5f95913131d8c501d5f917
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-22 18:25:29 -0500

    Cleaned up Main component for further development

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,7 @@
 import React from 'react'
-import Header from './Header'
-import MainContent from './MainContent'
-import Registration from './Registration'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const Main = () => {
-    console.log("Something here?")
     return (
-      // <Router>
-      //   <div>
-      //     {/* <Header /> */}
-      //     {/* <Switch>
-      //       <Route exact path="/register" component={Registration} />
-      //       <Route exact path="/home" component={MainContent} />
-      //     </Switch>     */}
-          
-      //   </div>
-      // </Router>
       <div>
         Main stuff
       </div>

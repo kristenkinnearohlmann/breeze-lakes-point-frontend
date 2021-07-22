e0f1ecd8dc725bb5c1b36b434b206ef0b3b37495
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-22 18:43:22 -0500

    Added Header css and started work on Header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <header>
             Header
-        </div>
+        </header>
     )
 }
 

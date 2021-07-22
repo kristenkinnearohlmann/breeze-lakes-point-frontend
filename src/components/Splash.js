14c73cac85c5c4a748878cfc352003cf9d090c7b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-21 22:11:40 -0500

    Update App css to consistent background color

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -8,7 +8,7 @@ const Main = () => {
           <img src={logo} className="app-logo" alt="logo" />
           <div>
             <span className="title brand-name">Breeze Lakes Point</span><br/>
-            <span className="title">serious fun!</span>
+            <span className="title">serious fun!</span><br/>
             <Link to="/home">Home</Link>
           </div>
       </div>

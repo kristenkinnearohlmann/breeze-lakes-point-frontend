e1fab67afc8359c44f56743d109223920af5777b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-26 23:16:11 -0500

    Centered Splash content

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,11 +6,13 @@ import './Splash.css'
 const Main = () => {
     return (
         <div className="app-splash">
-          <img src={logo} className="app-logo" alt="logo" />
-          <div>
-            <span className="title brand-name">Breeze Lakes Point</span><br/>
-            <span className="title">serious fun!</span><br/>
-            <Link to="/home">Home</Link>
+          <div className="app-splash-content">
+            <img src={logo} className="app-logo" alt="logo" />
+            <div>
+              <span className="title brand-name">Breeze Lakes Point</span><br/>
+              <span className="title">serious fun!</span><br/>
+              <Link to="/home">Home</Link>
+            </div>
           </div>
       </div>
     )

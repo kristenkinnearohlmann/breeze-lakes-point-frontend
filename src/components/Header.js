ce05b7792585ef7ca2c88779e24f69aa4d5d7770
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-07-30 21:33:05 -0500

    Starting work on Header display

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,18 @@ import './Header.css'
 const Header = () => {
     return (
         <header>
-            Logo
-            Links
-            Search
+            <div>
+                Logo
+            </div>
+            <div>
+                Links
+            </div>
+            <div>
+                Search
+            </div>
+            <div>
+                Login
+            </div>
         </header>
     )
 }

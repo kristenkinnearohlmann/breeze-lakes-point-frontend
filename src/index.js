f1ae88fae3e7461a16e738e8055b2beb974b8139
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-04 21:40:04 -0500

    Implemented base Redux and Thunk functionality

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

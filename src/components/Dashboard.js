5f38b08578a425cf8b9fb6fb99f90940d8863fac
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-05 18:19:40 -0500

    Converted Dashboard to class, connected to state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import { React, Component } from 'react'
+import { connect } from 'react-redux'
 
-const Dashboard = () => {
-    return (
-        <div>
-            <h1 className="title">Dashboard</h1>
-            <p>
-                <ul>
-                    <li>Applicant</li>
-                    <li>Editor</li>
-                    <li>FullEditor</li>
-                    <li>Admin</li>
-                    <li>Entity</li>
-                </ul>
-            </p>
-        </div>
-    )
+class Dashboard extends Component {
+    render () {
+        console.log('Here?')
+        console.log(this.props.currentUserId)
+        return (
+            <div>
+                <h1 className="title">Dashboard</h1>
+                <p>
+                    <ul>
+                        <li>Applicant</li>
+                        <li>Editor</li>
+                        <li>FullEditor</li>
+                        <li>Admin</li>
+                        <li>Entity</li>
+                    </ul>
+                </p>
+            </div>
+        )
+    }
 }
 
-export default Dashboard
+const mapStateToProps = state => {
+    return {
+        currentUserId: state.current_user.id
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)

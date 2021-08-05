c2873a12d65b051c929c7d57d86e2bb4ae823ae3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-05 18:05:43 -0500

    Sketched list of potential roles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,15 @@ const Dashboard = () => {
     return (
         <div>
             <h1 className="title">Dashboard</h1>
+            <p>
+                <ul>
+                    <li>Applicant</li>
+                    <li>Editor</li>
+                    <li>FullEditor</li>
+                    <li>Admin</li>
+                    <li>Entity</li>
+                </ul>
+            </p>
         </div>
     )
 }

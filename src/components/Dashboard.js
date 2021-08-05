489e51a147c000265ffa9c5b1f70eb1ce670c6a3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-05 18:23:38 -0500

    Added function to display dashboard items with default verbiage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,18 +5,26 @@ class Dashboard extends Component {
     render () {
         console.log('Here?')
         console.log(this.props.currentUserId)
+
+        const displayItems = () => {
+            if (this.props.currentUserId === -1) {
+                return <p>You must log in to see your dashboard.</p>
+            } else {
+                return <p>Current user: {this.props.currentUserId}</p>
+            }
+        }
+
         return (
             <div>
                 <h1 className="title">Dashboard</h1>
-                <p>
-                    <ul>
-                        <li>Applicant</li>
-                        <li>Editor</li>
-                        <li>FullEditor</li>
-                        <li>Admin</li>
-                        <li>Entity</li>
-                    </ul>
-                </p>
+                {displayItems()}
+                <ul>
+                    <li>Applicant</li>
+                    <li>Editor</li>
+                    <li>FullEditor</li>
+                    <li>Admin</li>
+                    <li>Entity</li>
+                </ul>
             </div>
         )
     }

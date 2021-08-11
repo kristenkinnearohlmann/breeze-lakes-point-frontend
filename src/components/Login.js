8e56edac497db3dbd3ad35928d0b09b900f1b30d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-10 20:14:20 -0500

    Added local state for form and form tags

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,19 @@
 import { React, Component } from 'react'
 
 class Login extends Component {
+    
+    state = {
+        email: ''
+    }
+
     render() {
+        console.log(this.state.email)
         return (
             <div>
                 Login form will go here
+                <form>
+                    
+                </form>
             </div>
         )
     }

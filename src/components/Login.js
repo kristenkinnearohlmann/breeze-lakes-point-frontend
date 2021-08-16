a1070b9c0c5e3548a8570f729ff7dbc7fac26d6e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-15 21:46:19 -0500

    Added import statement for loginUser action to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {connect } from 'react-redux'
+import { loginUser } from '../actions/rootActions'
 import './Login.css'
 
 class Login extends Component {

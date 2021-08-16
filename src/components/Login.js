0727b32c2f2ec4eb3d4f698a35979181bf7408d0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-15 21:24:48 -0500

    Added onChange and Submit event handlers and attributes to form fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,22 +4,34 @@ import './Login.css'
 class Login extends Component {
     
     state = {
-        email: ''
+        email: '',
+        password: ''
     }
 
     render() {
         console.log(this.state.email)
 
+        const handleOnChange = event => {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
+
+        const handleSubmit = event => {
+            event.preventDefault()
+            console.log(this.state)
+        }
+
         return (
             <div>
-                <form className="form-style">
+                <form className="form-style" onSubmit={this.handleSubmit}>
                     <p>
                         <label name="email">Email</label><br/>
-                        <input type="text" />
+                        <input type="text" name="email" value={this.state.email} onChange={this.handleOnChange} />
                     </p>
                     <p>
                         <label name="password">Password</label><br/>
-                        <input type="password" />
+                        <input type="password" name="password" value={this.state.password} onChange={this.handleOnChange} />
                     </p>
                     <input type="submit" />
                 </form>

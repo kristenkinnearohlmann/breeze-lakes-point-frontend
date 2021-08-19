306b4a06155937d2bd51bfa2e4f60caad78b80d5
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-18 21:40:09 -0500

    Completed switch for MVP login demo

--- src/actions/rootActions.js
+++ src/actions/rootActions.js
@@ -2,8 +2,27 @@ const loginUser = loginParms => {
     return (dispatch) => {
 
         switch (loginParms.loginUser) {
+            case 'user@mail.com':
+                console.log('User')
+                break
+            case 'desk@mail.com':
+                console.log('Desk')
+                break
+            case 'editor@mail.com':
+                console.log('Editor')
+                break
+            case 'manager@mail.com':
+                console.log('Manager')
+                break
+            case 'admin@mail.com':
+                console.log('Admin')
+                break
+            case 'vendor@mail.com':
+                console.log('Vendor')
+                break
             default:
                 console.log(loginParms.email)
+                break
         }
         dispatch( {type: 'LOGIN', payload: loginParms} )
     }

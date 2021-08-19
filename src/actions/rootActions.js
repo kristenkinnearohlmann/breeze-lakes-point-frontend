001c820b9ac6b459796e4e98ca494a2531d97911
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-18 21:48:16 -0500

    Moved object update after switch statement to more efficiently update the user id for loginUser action

--- src/actions/rootActions.js
+++ src/actions/rootActions.js
@@ -1,35 +1,45 @@
 const loginUser = loginParms => {
     return (dispatch) => {
 
+        let userId
+
         switch (loginParms.email) {
             case 'user@mail.com':
                 console.log('User')
+                userId = 2
                 break
             case 'desk@mail.com':
                 console.log('Desk')
+                userId = 3
                 break
             case 'editor@mail.com':
                 console.log('Editor')
+                userId = 4
                 break
             case 'manager@mail.com':
                 console.log('Manager')
+                userId = 5
                 break
             case 'admin@mail.com':
                 console.log('Admin')
+                userId = 6
                 break
             case 'vendor@mail.com':
                 console.log('Vendor')
+                userId = 7
                 break
             case 'sysadmin@mail.com':
                 console.log('SysAdmin')
-                loginParms = {
-                    ...loginParms,
-                    currentUserId: 1
-                }
+                userId = 1
                 break
             default:
                 console.log(loginParms.email)
                 break
+        }
+
+        loginParms = {
+            ...loginParms,
+            currentUserId: userId
         }
         dispatch( {type: 'LOGIN', payload: loginParms} )
     }

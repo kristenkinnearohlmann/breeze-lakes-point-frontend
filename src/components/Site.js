1b507ea6e96b9c63d31766edafdd734b168bc4f0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-23 22:20:09 -0500

    Checking value of state in Site component

--- src/components/Site.js
+++ src/components/Site.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Splash from './Splash'
 
-const Site = () => {
-
+const Site = props => {
+    console.log(props.currentUserId)
     return (
       <div>
         <Splash />

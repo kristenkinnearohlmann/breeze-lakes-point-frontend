173ba292355fee9daa6659412869357a23e1b9c3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-27 23:47:35 -0500

    Started work on filling drop-down with state abbreviations:

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -33,6 +33,9 @@ class Registration extends Component {
         console.log('Registration component')
         console.log(this.props.currentUserId)   
         const state_abbrevs = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ]
+        state_abbrevs.forEach(abbrev => {
+            console.log(`<option value='${abbrev}'></option>`)
+        })
         return (
             <div>
                 <h2>Registration</h2>
